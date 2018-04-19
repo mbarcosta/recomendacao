639e1973dc35eb3b2d54acca5f13070f5a8c05c5
eribeiro <eribeiro@SR4933L>
2018-04-19 13:51:11 -0300

    Sala 715

--- src/app_gs_1.py
+++ src/app_gs_1.py
@@ -3,17 +3,19 @@ import tadgroupset as gs
 def main():
     str_val_cs = "A, B, C, D"
 
-    expgsA = tadexpgs.create("ABCD ou (EFG xor HIJ)")
+    #expgsA = tadexpgs.create("ABCD ou (EFG xor HIJ)")
 
     #print(expgsA) --> GS1 = ABCD ou EFGHIJ e GS2 = EFG xor HIJ
 
 
 
-    gsA = gs.create("ABCD ou (EFG xor HIJ)")
+    #gsA = gs.create("ABCD ou (EFG xor HIJ)")
 
-    ou
+    #ou
 
-    gsA = gs.create("ABCD ou EFGHIJ e GS2 = EFG xor HIJ")
+    #gsA = gs.create("ABCD ou EFGHIJ e GS2 = EFG xor HIJ")
+    
+   # Protótipo para desenvolvimento.
 
 
     # retorna GS1 = ou ABCD EFGHIJ
