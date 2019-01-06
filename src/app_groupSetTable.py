37a38c36490609f0a51d2070de8b87ad0af7fbf1
mbarcosta <33872670+mbarcosta@users.noreply.github.com>
2019-01-06 07:19:26 -0200

    Definição do tadProcessSPecification -Mateus - Casa - 06/01

--- src/app_groupSetTable.py
+++ src/app_groupSetTable.py
@@ -5,26 +5,35 @@ import tadGroupSetTable as gst
 
 def main():
     gstable = gst.create()
-    gs_A = gs.create_exp("t0,t1 xor t2,t3,t8,t9,t10")
-    gs_D = gs.create_exp("t2 or t3")
-    gs_B = gs.create_exp("t5 or t6")
-    gs_C = gs.create_exp("t0 or t1")
-    gs_E = gs.create_exp("t2,t3 xor t8,t9,t10")
-    gs_F = gs.create_exp("t8 xor t9,t10")
-    gs_G = gs.create_exp("t9 or t10")
+   # gs_A = gs.create_exp("t0,t1 xor t2,t3,t8,t9,t10")
+    #gs_D = gs.create_exp("t2 or t3")
+    #gs_B = gs.create_exp("t5 or t6")
+   # gs_C = gs.create_exp("t0 or t1")
+#    gs_E = gs.create_exp("t2,t3 xor t8,t9,t10")
+ #   gs_F = gs.create_exp("t8 xor t9,t10")
+ #   gs_G = gs.create_exp("t9 or t10")
 
-    gstable = gst.addGS(gstable, gs_A)     
+    gs_1 = gs.create_exp("t1,t2 xor t3,t4")
+    gs_2 = gs.create_exp("t1 xor t2")
+    gs_3 = gs.create_exp("t3 or t4")
+    gstable = gst.addGS(gstable, gs_1)
+    gstable = gst.addGS(gstable, gs_2)
+    gstable = gst.addGS(gstable, gs_3)
+
+    # gstable = gst.addGS(gstable, gs_A)
     
-    gstable = gst.addGS(gstable, gs_C)
-    gstable = gst.addGS(gstable, gs_B)
+    #  gstable = gst.addGS(gstable, gs_C)
+    #gstable = gst.addGS(gstable, gs_B)
     
     #     gstable = gst.addGS(gstable, gs_D)
     #     gstable = gst.addGS(gstable, gs_E)
     #     gstable = gst.addGS(gstable, gs_F)
     #     gstable = gst.addGS(gstable, gs_G)
-
-    gst.printTabGS(gstable)
-
+    print("Vamos lá")
+    #gst.printTabGS(gstable)
+    print(len(gstable))
+    print(gstable[0])
+    print(gstable[1])
     return 0
 
 
